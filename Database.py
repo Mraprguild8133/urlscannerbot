d6fbc28167970f1c1aaf9284bbbba6af51bce911
Mraprguild8133 <mraprguild06@gmail.com>
2025-08-30 23:32:48 +0530

    Delete Database.py

--- Database.py
+++
@@ -1,9 +0,0 @@
-class Database:
-    def is_connected(self):
-        try:
-            # Simple ping or query
-            self.client.admin.command('ping')
-            return True
-        except:
-            return False
-          
