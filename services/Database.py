16c20cba898f8a44486381691360c02910e9429c
Mraprguild8133 <mraprguild06@gmail.com>
2025-08-30 23:23:50 +0530

    Update Database.py

--- services/Database.py
+++ services/Database.py
@@ -1,1 +1,9 @@
-
+class Database:
+    def is_connected(self):
+        try:
+            # Simple ping or query
+            self.client.admin.command('ping')
+            return True
+        except:
+            return False
+          
